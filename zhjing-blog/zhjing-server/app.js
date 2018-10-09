1217064156b686972fb26074400537ff6a50f3d4
zhangjing <zhjing1019@gmail.com>
2018-10-09 14:58:29 +0800

    nodejs

--- zhjing-blog/zhjing-server/app.js
+++ zhjing-blog/zhjing-server/app.js
@@ -1,27 +1,35 @@
+//引入依赖包
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//引入路由文件
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//创建应用实例
 var app = express();
 
+//设置视图目录和模板引擎
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 以下皆为注册中间件
+// 内置中间件
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 路由中间件
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// 404错误处理中间件
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,4 +46,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// 导出app实例对象
 module.exports = app;
--- zhjing-blog/zhjing-server/routes/index.js
+++ zhjing-blog/zhjing-server/routes/index.js
@@ -1,7 +1,9 @@
+// 引入依赖包
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// 获取首页
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
