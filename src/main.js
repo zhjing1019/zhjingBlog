88d3d246dab7c1433674bab0bb16386338d57936
张静 <zhjing1019@gmail.com>
2019-09-01 11:23:10 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/display.css';
+import axios from 'axios'
 
+Vue.use(axios)
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
